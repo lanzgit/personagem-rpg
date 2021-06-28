a48c4a087366dfe3f92f660263ac87f2749ac5d8
ViNiCiUs <vnvianna@icloud.com>
2021-06-28 14:33:17 -0300

    initial commit

---
+++ src/test/java/br/edu/infnet/personagemrpg/PersonagemrpgApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.infnet.personagemrpg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonagemrpgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
